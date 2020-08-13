74e119ff2cf5084a9146ef579513c864c178b05b
Junior Rivas <jrtimmobilier@gmail.com>
2020-08-13 17:02:35 -0400

    start the cocktail controller

--- db/seeds.rb
+++ db/seeds.rb
@@ -1,6 +1,20 @@
 # This file should contain all the record creation needed to seed the database with its default values.
 # The data can then be loaded with the rails db:seed command (or created alongside the database with db:setup).
-#
+#Ingredient.create(name: "lemon")
+Ingredient.create(name: "ice")
+Ingredient.create(name: "mint leaves")
+Ingredient.create(name: "orange")
+Ingredient.create(name: "lemon")
+Ingredient.create(name: "apple")
+Ingredient.create(name: "lime")
+Ingredient.create(name: "peach")
+Ingredient.create(name: "basilic")
+Ingredient.create(name: "orange juice")
+Ingredient.create(name: "soda")
+Ingredient.create(name: "coke")
+Ingredient.create(name: "7up")
+Ingredient.create(name: "coffee")
+Cocktail.create(name: "zombie")
 # Examples:
 #
 #   movies = Movie.create([{ name: 'Star Wars' }, { name: 'Lord of the Rings' }])
